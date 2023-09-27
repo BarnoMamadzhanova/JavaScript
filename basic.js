f1b4625a8d77fe84c48bbeefbd4429a6cf9fe026
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 11:29:21 +0200

    Intermediate Exercise 1

--- basic.js
+++ basic.js
@@ -1,2 +1,9 @@
 // console.log("Hi my name is Martin");
 
+// Intermediate Exercise 1
+
+// const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+// cars.sort();
+// console.log(cars);
+
+// Intermediate Exercise 2
