630a6bdecb7243932a97c74f914ab050df6d46c4
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 12:12:21 +0200

    Intermediate Exercise 3

--- basic.js
+++ basic.js
@@ -15,9 +15,23 @@
 // fruits.pop();
 // console.log(fruits);
 
-const animals = ["monkey", "horse", "dog"];
-console.log(animals);
-animals.sort();
-console.log(animals);
-animals.unshift("cat");
-console.log(animals);
+// const animals = ["monkey", "horse", "dog"];
+// console.log(animals);
+// animals.sort();
+// console.log(animals);
+// animals.unshift("cat");
+// console.log(animals);
+
+// Intermediate Exercise 3
+
+let fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+// let fruits1 = "mango \n cherries \n kiwi \n grapes \n pear \n peach \n orange \n lemon";
+
+// const fArray = fruits1.split("\n");
+const fArray = fruits.split("/");
+document.write(`${fArray[0]} <br> ${fArray[1]} <br> ${fArray[2]} <br> ${fArray[3]} <br> ${fArray[4]} <br> ${fArray[5]} <br> ${fArray[6]} <br> ${fArray[7]}`);
+
+
+
+
+
