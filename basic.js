c0f2a7eb203b2b26e078518372c364628559c70c
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 11:37:21 +0200

    Intermediate Exercise 2

--- basic.js
+++ basic.js
@@ -7,3 +7,17 @@
 // console.log(cars);
 
 // Intermediate Exercise 2
+
+// const fruits = ["apple", "banana", "kiwi"];
+// console.log(fruits);
+// fruits.push("orange");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+const animals = ["monkey", "horse", "dog"];
+console.log(animals);
+animals.sort();
+console.log(animals);
+animals.unshift("cat");
+console.log(animals);
