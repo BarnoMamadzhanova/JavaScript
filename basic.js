10d2f7a9209bac0a8627f816fb128ac76780552b
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 13:14:43 +0200

    Advanced Exercise 1 finish

--- basic.js
+++ basic.js
@@ -24,14 +24,34 @@
 
 // Intermediate Exercise 3
 
-let fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
-// let fruits1 = "mango \n cherries \n kiwi \n grapes \n pear \n peach \n orange \n lemon";
+// let fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
 
-// const fArray = fruits1.split("\n");
-const fArray = fruits.split("/");
-document.write(`${fArray[0]} <br> ${fArray[1]} <br> ${fArray[2]} <br> ${fArray[3]} <br> ${fArray[4]} <br> ${fArray[5]} <br> ${fArray[6]} <br> ${fArray[7]}`);
+// const fArray = fruits.split("/");
+// document.write(`${fArray[0]} <br> ${fArray[1]} <br> ${fArray[2]} <br> ${fArray[3]} <br> ${fArray[4]} <br> ${fArray[5]} <br> ${fArray[6]} <br> ${fArray[7]}`);
+
+// Advanced Exercise 1
+
+let a = 7;
+
+let b = 14;
+
+let c = "21";
+let c1 = Number(c);
+let d = '36';
+let d1 = Number(d);
+let e = 42;
+
+let z = a+b+c1+d1+e; 
 
 
+let f = '1';
+let f1 = Number(f);
+let g = 15;
+let h = 8;
+let i = "1";
+let i1 = Number(i);
+let y = f1*g*h*i1;
 
-
+let x = z/y;
+document.write(x);
 
