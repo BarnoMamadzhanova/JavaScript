c42f771930d04334501b2bc1325b830838387745
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 14:05:17 +0200

    Challenge end

--- basic.js
+++ basic.js
@@ -113,8 +113,17 @@
 
 // Advanced Exercise 3
 
-const numbers = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+// const numbers = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
 
-// Select and output the numbers 11, 25, 17, 27 and 0 in the console.
+// // Select and output the numbers 11, 25, 17, 27 and 0 in the console.
 
-console.log(numbers[1][1], numbers[4][2], numbers[5][3], numbers[2][3], numbers[2][1]);
+// console.log(numbers[1][1], numbers[4][2], numbers[5][3], numbers[2][3], numbers[2][1]);
+
+
+// Challenge
+
+let string = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+let arr = new Array();
+arr = string.split('$');
+console.log(arr);
+console.log(arr.join(' '));
