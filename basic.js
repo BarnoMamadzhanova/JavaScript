72f8e13b334a8e33521b4280ecea8c40b6c229d5
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-27 13:41:35 +0200

    Advanced Exercise 2 finished

--- basic.js
+++ basic.js
@@ -31,27 +31,85 @@
 
 // Advanced Exercise 1
 
-let a = 7;
+// let a = 7;
 
-let b = 14;
+// let b = 14;
 
-let c = "21";
-let c1 = Number(c);
-let d = '36';
-let d1 = Number(d);
-let e = 42;
+// let c = "21";
+// let c1 = Number(c);
+// let d = '36';
+// let d1 = Number(d);
+// let e = 42;
 
-let z = a+b+c1+d1+e; 
+// let z = a+b+c1+d1+e; 
 
 
-let f = '1';
-let f1 = Number(f);
-let g = 15;
-let h = 8;
-let i = "1";
-let i1 = Number(i);
-let y = f1*g*h*i1;
+// let f = '1';
+// let f1 = Number(f);
+// let g = 15;
+// let h = 8;
+// let i = "1";
+// let i1 = Number(i);
+// let y = f1*g*h*i1;
 
-let x = z/y;
-document.write(x);
+// let x = z/y;
+// document.write(x);
+
+// Advanced Exercise 2
+
+// 1.     Remove "Greg" from the array.
+// let people = ["Greg", "Mary", "Devon","James"];
+// const remowed = people.shift();
+// people.splice(0,1);
+// console.log(people);
+
+
+// 2.     Add "Matt" to the front of the array.
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// people.unshift("Matt");
+// console.log(people);
+
+// 3.     Remove “James” from the array.
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// const removed = people.pop();
+// console.log(people);
+
+// 4.     Add your name to the end of the array.
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// people.push("Barno");
+// console.log(people);
+
+// 5.     Make a copy of the array using slice. The copy should NOT include "Mary" or "Matt".
+
+// let people = ["Matt", "Greg", "Mary", "Devon","James"];
+// console.log(people.slice(1,5));
+
+// 6.     Using the indexOf method, find the index of where "Mary" is located.
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// console.log(people.indexOf('Mary'));
+
+// 7.     Using the indexOf method, find the index of where "Foo" is located (this should return -1).
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// console.log(people.indexOf('Foo'));
+
+// 8.     Redefine the people variable with the values you started with.
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// console.log(people);
+
+
+// 9.     Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Anna".
+//  Your array should look like this when you are done ["Greg", "Mary", "Elizabeth", "Anna", "James"].
+
+// let people = ["Greg", "Mary", "Devon","James"];
+// people.splice(2, 1, "Elizabeth", "Anna");
+// console.log(people);
+
+
+
 
